e29402a67e90b49659e148cd38507e0921682926
Hiba <eng.lilil92@gmail.com>
2022-10-11 23:33:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(para){
+    if(para != undefined)
+    return `This Saturday, I want to ${para}!`;
+    else return `This Saturday, I want to roller-skate!`;
+}
+console.log(saturdayFun("just relax"));
+
+
+function mondayWork(para1, para2){
+    if(para1 != undefined ) 
+  return `This Monday, I will ${para1}.`;
+  else {
+    para1 = 'go to the office';
+    return `This Monday, I will ${para1}.`  
+  }
+}
+ console.log(mondayWork("clean the home"));
+
+
+ function wrapAdjective(s="*"){
+    return function(adj="special")
+    {
+        return `You are ${s}${adj}${s}!`;
+    }
+}
+
+//console.log(wrapAdjective("%")("a dedicated programmer"))
+
